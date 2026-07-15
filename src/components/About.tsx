7fc78d47fdeb55604f0a67c380c528cf95c1ce8d
Kat Hernandez <katreen.hernandez@synacy.com>
2026-07-15 16:49:42 +0800

    Fix gap in About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,14 +5,10 @@ export function About() {
     <div>
       <section
         id="about"
-        className="px-7 lg:px-24 2xl:px-0 mt-12 2xl:mt-60 flex flex-col gap-2"
+        className="px-7 lg:px-24 2xl:px-0 mt-12 2xl:mt-60 flex flex-col gap-6"
       >
-        <p>
-          <Placeholder rows={6} />
-        </p>
-        <p>
-          <Placeholder rows={3} />
-        </p>
+        <Placeholder rows={6} />
+        <Placeholder rows={3} />
       </section>
     </div>
   );
